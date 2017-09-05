f77512b6194a4c80ab95f46924529410a7458e9f
pkuptcov <pavel.kuptcov@gmail.com>
2017-09-05 17:33:08 +0300

    v1.00

--- desktop/master/regress/Chrome/ur_delivery_auth.py
+++ desktop/master/regress/Chrome/ur_delivery_auth.py
@@ -7,7 +7,6 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
-
         # Подключение вебдрайвера и конфиг окружения
 class ur_delivery_auth(unittest.TestCase):
     def setUp(self):
